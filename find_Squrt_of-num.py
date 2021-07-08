fe0aa85e621199759bb669ce62cab38fbe9d8ef4
anokhramesh <67797819+anokhramesh@users.noreply.github.com>
2021-07-08 13:26:05 +0400

    Update find_Squrt_of-num.py

--- find_Squrt_of-num.py
+++ find_Squrt_of-num.py
@@ -1,6 +1,6 @@
 # This is simple program to find the suare root of a number.
 import math# importing mats module   
 s =""# s is a variable to store the user input value.
-while s != "0":# create an ifinite loop,if user input is 0 quit the program
-    s = float(input("Enter a number to find Sqrt or enter 0 for exit\n"))
+while s != "q":# create an ifinite loop,if user input is q quit the program
+    s = float(input("Enter a number to find Sqrt or enter q for quit\n"))
     print ("Square root of ",s,"is =",math.sqrt(s))
